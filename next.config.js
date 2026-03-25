6255e97840392d14141c83bc86e710d7acaee3be
KavinduAluthwaththa <Kavindu18602@gmail.com>
2026-03-25 12:50:34 +0530

    chore(db-docs): consolidate migrations and finalize about/brand updates

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'wpvauifxcraxchmbzlqb.supabase.co',
+      },
+      {
+        protocol: 'https',
         hostname: 'bolt.new',
       },
     ],
